853e9b99479aee5ff603ffc84fd9991e9f138a77
ale <alexis.borges@estudiantes.utec.edu.uy>
2023-08-10 18:33:48 -0300

    Main Vacío Calsón Caliente

---
+++ src/Pelicula.java
@@ -0,0 +1,5 @@
+public class Pelicula extends Produccion {
+    public Pelicula(int id, String nombre, String descripción, double duracion, double precioPorHora) {
+        super(id, nombre, descripción, duracion, precioPorHora);
+    }
+}
