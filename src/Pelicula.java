8183c4436faeef9dd8489f735db668a8582ec58e
ale <alexis.borges@estudiantes.utec.edu.uy>
2023-08-11 15:00:52 -0300

    pasado a maven

--- src/Pelicula.java
+++
@@ -1,5 +0,0 @@
-public class Pelicula extends Produccion {
-    public Pelicula(int id, String nombre, String descripción, double duracion, double precioPorHora) {
-        super(id, nombre, descripción, duracion, precioPorHora);
-    }
-}
--- src/Serie.java
+++
@@ -1,5 +0,0 @@
-public class Serie extends Produccion{
-    public Serie(int id, String nombre, String descripción, double duracion, double precioPorHora) {
-        super(id, nombre, descripción, duracion, precioPorHora);
-    }
-}
