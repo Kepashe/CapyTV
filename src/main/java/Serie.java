d1b4626b2b2d7399e92f406c59ef57f9e8eebcc1
ale <alexis.borges@estudiantes.utec.edu.uy>
2023-08-11 15:01:20 -0300

    pasado a maven

---
+++ src/main/java/Serie.java
@@ -0,0 +1,5 @@
+public class Serie extends Produccion{
+    public Serie(int id, String nombre, String descripción, double duracion, double precioPorHora) {
+        super(id, nombre, descripción, duracion, precioPorHora);
+    }
+}
---
+++ src/main/java/iPlataforma.java
@@ -0,0 +1,10 @@
+public interface iPlataforma {
+    String informe();
+    void agregarSeries(Serie serie);
+    void agregarPaquetes(Paquete paquete);
+    void agregarPeliculas(Pelicula pelicula);
+    void eliminarPelicula(Pelicula pelicula);
+    void eliminarSerie(Serie serie);
+    void eliminarPaquete(Paquete paquete);
+    void alquilar(Produccion produccion);
+}
